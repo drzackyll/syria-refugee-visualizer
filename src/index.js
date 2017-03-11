631696be12aa471419cf83de9145be386407fd65
drzackyll <adams.z.d@gmail.com>
2017-03-11 12:55:18 -0500

    Adds working geocode api

--- src/index.js
+++ src/index.js
@@ -3,20 +3,41 @@ import ReactDOM from 'react-dom'
 import SearchBar from './components/search_bar'
 import Visualizer from './components/visualizer'
 import $ from 'jquery'
-const API_KEY = "5aa83bd6f3a03ec298c39f3309e80a6ca1686b4b"
+const CENSUS_API_KEY = "5aa83bd6f3a03ec298c39f3309e80a6ca1686b4b"
+const GOOGLE_API_KEY = "AIzaSyAsoiAg1cBZUnoQOM5xuTvV6VxHPf3V6_k"
 
 class App extends Component {
   constructor(props) {
     super(props)
 
-    this.state = { population: "", representatives: {}, location: ""}
+    this.state = { population: "", representatives: {}, zip: "", cityState: { city: "", state: "" } }
   }
 
-  populationSearch(location) {
-    this.setState({ location })
-    if (location.length === 5) {
+  locationSearch(zip) {
+    if (zip.length === 5) {
       $.ajax({
-        url: `https://api.census.gov/data/2013/acs5?get=NAME,B01001_001E&for=zip+code+tabulation+area:${location}&key=${API_KEY}`,
+        url: `https://maps.googleapis.com/maps/api/geocode/json?address=${zip}&key=${GOOGLE_API_KEY}`,
+        type: "GET",
+        dataType: "json"
+      }).then(
+        response => this.setState({
+          cityState: {
+            city: response.results[0].address_components[1].long_name,
+            state: response.results[0].address_components[2].long_name
+          }
+        })
+      )
+    } else {
+      this.setState({ cityState: { city: "", state: "" } })
+    }
+    this.populationSearch(zip)
+  }
+
+  populationSearch(zip) {
+    this.setState({ zip })
+    if (zip.length === 5) {
+      $.ajax({
+        url: `https://api.census.gov/data/2013/acs5?get=NAME,B01001_001E&for=zip+code+tabulation+area:${zip}&key=${CENSUS_API_KEY}`,
         type: "GET",
         dataType: "json"
       }).then(
@@ -25,13 +46,13 @@ class App extends Component {
     } else {
       this.setState({ population: ""})
     }
-    this.representativeSearch(location)
+    this.representativeSearch(zip)
   }
 
-  representativeSearch(location) {
-    if (location.length === 5) {
+  representativeSearch(zip) {
+    if (zip.length === 5) {
       $.ajax({
-        url: `https://congress.api.sunlightfoundation.com/legislators/locate?zip=${location}`,
+        url: `https://congress.api.sunlightfoundation.com/legislators/locate?zip=${zip}`,
         type: "GET",
         dataType: "json"
       }).then(
@@ -45,8 +66,8 @@ class App extends Component {
   render() {
     return (
       <div>
-        <SearchBar  reps={this.state.representatives} onLocationChange={location => this.populationSearch(location)} />
-        <Visualizer population={this.state.population} location={this.state.location} />
+        <SearchBar  reps={this.state.representatives} onLocationChange={zip => this.locationSearch(zip)} />
+        <Visualizer population={this.state.population} zip={this.state.zip} />
       </div>
     );
   }
